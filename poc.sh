fee49d53c55311653cdf733139c4e0cdf621b609
hawk3yE <48472370+Gumbeoketch@users.noreply.github.com>
2023-06-07 13:16:28 +0300

    Update poc.sh

--- poc.sh
+++ poc.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-git clone https: //github.com/DefectDojo/django-DefectDojo
+git clone https://github.com/DefectDojo/django-DefectDojo
 cd django-DefectDojo
 # building
-dc-build.sh
+bash dc-build.sh
 
 # running
-dc-up.sh
+bash dc-up.sh
 
 #go to http://localhost:8080 on your browser
+#admin:admin
 
 #running sonarqube docker image
 docker run -d -p 9000:9000 sonarqube
 
 #go to http://localhost:9000 on your browser
+#admin:admin and you will be prompted for new password
 #under account>security generate user token/login key
 #under project generate project token/key
 
@@ -35,3 +37,4 @@ docker run \
     -e SONAR_LOGIN="$loginkey" \
     -v "$dir:/usr/src" \
     sonarsource/sonar-scanner-cli
+
